8b85de9c0e46951ba2a79606ca86a638a4110cdb
Wei <2558939179@qq.com>
2022-03-27 00:32:43 +0800

    come true the feature when you click the place card this app will show the weather about this place,and more feather about the drawer

--- app/src/main/java/com/weiweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/weiweather/android/ui/weather/WeatherActivity.kt
@@ -13,6 +13,7 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.sunnyweather.android.logic.dao.PlaceDao
 import com.sunnyweather.android.logic.model.getSky
 import com.weiweather.android.R
 import com.weiweather.android.WeiWeatherApplication.Companion.showToast
@@ -29,9 +30,6 @@ class WeatherActivity : AppCompatActivity() {
     val viewModel by lazy{ ViewModelProvider(this).get(WeatherViewModel::class.java)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val decorView=window.decorView
-        decorView.systemUiVisibility=View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        window.statusBarColor= Color.TRANSPARENT
         setContentView(R.layout.activity_weather)
         if(viewModel.locationLng.isEmpty()){
             viewModel.locationLng=intent.getStringExtra("location_lng")?:""
@@ -82,12 +80,11 @@ class WeatherActivity : AppCompatActivity() {
     }
 
     private fun showWeatherInfo(weather: Weather) {
-        val view: View = LayoutInflater.from(this).inflate(R.layout.place_item, null)
-        val placeName=view.findViewById(R.id.placeName) as TextView
-        placeName.text=viewModel.placeName
         val realtime=weather.realtime
         val daily=weather.daily
-
+        val savedPlaceName=PlaceDao.getSavedPlace().name
+        showOnePlace.text= savedPlaceName
+        ("你当前所在地方为:$savedPlaceName").showToast()
         val currentTempText="${realtime.temperature.toInt()} °C"
         currentTemp.text=currentTempText
         currentKsy.text= getSky(realtime.skycon).info
